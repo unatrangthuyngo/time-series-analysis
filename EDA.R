8c987be9db6c83ca91724ff425b360ab81428015
unatrangthuyngo <una.ngo005@hotmail.com>
2026-04-01 13:15:36 +1030

    EDA update (2)
    This file include an updated explanatory data anlalysis, where the moving average for the time series for both TLB and TFR are calculated in confirming the underlying trend of the time series plots. 
    
    Additionally certain sections were updated to further analyse of the time series included, such as an exploration of a log transform on both TLB and TFR data, supported for the BoxCox calculation in an attempt for stabilization of the variance, as well include stationary numerical test such as ADF and KPSS, in further testing the stationarity of the data. Updates as well of suspicions of certain appropriate models to fit the data.

--- EDA.R
+++ EDA.R
@@ -1,3 +1,4 @@
+install.packages("tseries")
 library(stringr)
 library(dplyr)
 library(tsibble)
@@ -5,6 +6,7 @@ library(feasts)
 library(fable)
 library(ggplot2)
 library(forecast)
+library(tseries)
 BAFA <- read.csv("BirthsAndFertilityRatesAnnual.csv")
 head(BAFA)
 summary(BAFA)
@@ -40,8 +42,11 @@ tlb_ts <- ts(tlb_df$TLB, start = 1960, frequency = 1)
 tfr_ts <- ts(tfr_df$TFR, start = 1960, frequency = 1)
 
 #EDA 
+
 summary(tlb_ts)
 summary(tfr_ts)
+sd(tlb_ts)
+sd(tfr_ts)
 
 #initial outlook of the variables to year 
 autoplot(tlb_ts) #long-term downward trend, seasonality does not seem obvious 
@@ -56,8 +61,19 @@ ggplot(df, aes (x= Year, y = TLB)) +
   ggtitle("Total Live Births")
 
 #Trend analysis
-#moving average to comfirm downward trend
+#moving average to comfirm downward trend, and possisble polynominal fitting regressisive model more flexibility and arima models 
 
+# TLB
+autoplot(tlb_ts) +
+  autolayer(ma(tlb_ts, order = 5), series = "5-year MA") +
+  ggtitle("TLB with Moving Average")
+
+# TFR
+autoplot(tfr_ts) +
+  autolayer(ma(tfr_ts, order = 5), series = "5-year MA") +
+  ggtitle("TFR with Moving Average")
+
+#Structural breaks in the data
 
 
 #time series decomposition 
@@ -72,15 +88,40 @@ plot(stl_decomp)
 #STL
 tfr_ts |> model(stl = STL(TFR)) |> components() |> autoplot()
 
-
+#STL decomposition fail as the data is evidiently not havign and seasonality component
 #Because the data are annual, seasonal decomposition using STL is not appropriate. The series do not contain within-year seasonal structure, so the temporal analysis should focus instead on trend, stationarity, autocorrelation, and structural change.
 
+
+#box-cox analysis 
+#installed to asses the need for varience stabilisation
+BoxCox.lambda(tlb_ts)
+BoxCox.lambda(tfr_ts)
+#potential log transformation on 
 #Stationary analysis 
 #Difference to achieve stationary
+#tfr
+adf.test(tfr_ts) #comfirms nonstationarity on raw data 
+kpss.test(tfr_ts) #comfrims nonstatioanrity
+
+adf.test(tlb_ts) #comfirms nonstatioanriy on raw data
+kpss.test(tlb_ts) #comfirms nonstationarity on raw data 
+#conflicting results in the test suggesting the implementation of the log transforms
+
+adf.test(log(tlb_ts)) 
+kpss.test(log(tlb_ts))
+
+#Difference to achieve stationary(visualisation) 
 autoplot(diff(tlb_ts))
 autoplot(diff(tfr_ts))
+autoplot(diff(log(tlb_ts))) #statioanrity definitely improved from log transform
+autoplot(diff(log(tfr_ts))) #statioanrity is seen as similar 
+#comfirming stationarity is achieve through diffirencing
+adf.test(diff(tfr_ts)) #staionrity is achecived through first order diffrencing
+kpss.test(diff(tfr_ts))
 
-#Correallation analysis
+adf.test(diff(tlb_ts))
+kpss.test
+#Correlation analysis
 #autocorrelation analysis
 #ACF and PACF plots
 Acf(diff(tfr_ts))
@@ -99,7 +140,7 @@ diff(diff(tfr_ts)) |> Acf() |> autoplot()
 
 #linear regression model with polynomial fitting
 #Ar model 
-#Arima model
+#ARIMA (non-seasonal)
 
 model_tlb <- 
 model_tfr <-
@@ -107,6 +148,7 @@ model_tfr <-
 
 breakpoints(tfr_ts ~ 1)
 
-#Trend modelling
-#Stationarity (differencing)
 #ARIMA (non-seasonal)
+#testing of forecasting of models
+#Split into training and test for different model
+#Research questions 1-3
