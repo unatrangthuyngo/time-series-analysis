7c3e3fa0f001c461341c016ee482f5398858a6c4
unatrangthuyngo <una.ngo005@hotmail.com>
2026-05-12 21:35:31 +0930

    Redoing EDA
    Given the feedback of my initial EDA i have restarted the process, in creating better time series models for both TLB and TFR. In this file it includes just the cleaning of the data of TLB and TFR, where the data is just extracted and transposed into variables from the original data. There is no further cleaning, given the data does not have missing values nor any other anomalies within the data.

---
+++ NewEDA.R
@@ -0,0 +1,42 @@
+library(stringr)
+library(dplyr)
+library(ggplot2)
+library(forecast)
+library(tseries)
+library(strucchange)
+library(fable)
+library(tsibble) 
+
+BAFA <- read.csv("BirthsAndFertilityRatesAnnual.csv")
+head(BAFA)
+summary(BAFA)
+#Filtering required Variables for time series analysis
+#TLB 
+TFR <- BAFA[1, ] 
+#TFR
+TLB <- BAFA[15, ]
+#Tranposing the rows to columns
+Tranposed_TLB <- t(TLB)
+Transposed_TFR <- t(TFR)
+
+# Help to convert the Tranposed_TLB to a data frame, and keeping the TLB numeric values
+tlb_df <- data.frame( 
+  Year = rownames(Tranposed_TLB)[-1], 
+  TLB = as.numeric(Tranposed_TLB[-1,1])
+)
+
+tfr_df <- data.frame( 
+  Year = rownames(Transposed_TFR)[-1], 
+  TFR = as.numeric(Transposed_TFR[-1,1])
+)
+
+tlb_df$Year <- as.numeric(str_remove(tlb_df$Year, "X"))
+tfr_df$Year <- as.numeric(str_remove(tfr_df$Year, "X"))
+
+tlb_df <- tlb_df[order(tlb_df$Year), ]
+tfr_df <- tfr_df[order(tfr_df$Year), ]
+
+df <- merge(tlb_df,tfr_df, by = "Year")
+
+tlb_ts <- ts(tlb_df$TLB, start = 1960, frequency = 1)
+tfr_ts <- ts(tfr_df$TFR, start = 1960, frequency = 1)
